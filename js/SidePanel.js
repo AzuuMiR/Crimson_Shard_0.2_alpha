b98df4e1001f549c40d3f2d19720efe43b0218a9
Quentin Berard <AzuuMiR@users.noreply.github.com>
2018-02-04 02:12:10 +0100

    Add files via upload

---
+++ js/SidePanel.js
@@ -0,0 +1,9 @@
+//Side Navigation Menu
+
+function openNav() {
+    document.getElementById("SidePanel").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("SidePanel").style.width = "0";
+}
