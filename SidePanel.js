e44bf5e4c0d4c6db95e9bfd5144f5c78d07a755f
Quentin Berard <AzuuMiR@users.noreply.github.com>
2018-02-04 02:12:24 +0100

    Delete SidePanel.js

--- SidePanel.js
+++
@@ -1,9 +0,0 @@
-//Side Navigation Menu
-
-function openNav() {
-    document.getElementById("SidePanel").style.width = "100%";
-}
-
-function closeNav() {
-    document.getElementById("SidePanel").style.width = "0";
-}
